fb12af2d557c5c0f97e38bcf74877cb79cf1df52
aaron777collins <aaron777collins@gmail.com>
2023-04-19 00:17:07 -0400

    added roles styles

--- src/pages/About/Roles/Roles.tsx
+++ src/pages/About/Roles/Roles.tsx
@@ -1,15 +1,15 @@
 import { Typography } from "@mui/material";
 import * as React from "react";
 import { PaddedCard } from "../../../components/PaddedCard";
+import "./Roles.scss";
 
 export interface IRolesProps {}
 
 export default function Roles(props: IRolesProps) {
   return (
     <PaddedCard>
-      <Typography variant="h2"> Roles:</Typography>
-      <ul>
-        {/* Manager, Researcher, Teaching Assistant, Student, Full-Stack Software Developer*/}
+      <Typography variant="h2">Roles:</Typography>
+      <ul className="roles-list">
         <li>Manager</li>
         <li>Researcher</li>
         <li>Teaching Assistant</li>
