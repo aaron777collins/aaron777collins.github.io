917e4115184560294895559a1b2a034f9a4a7025
Aaron Collins <aaron777collins@gmail.com>
2023-05-05 19:30:36 -0400

    fixed width of icon on the contact page

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -35,7 +35,7 @@ export default function ContactPage(props: IContactPageProps) {
                 <img
                   src="PhotoOfMeSquare.jpg"
                   style={{
-                    maxWidth: "100%",
+                    maxWidth: "250px",
                     height: "auto",
                     borderRadius: "50%",
                     minWidth: "200px",
